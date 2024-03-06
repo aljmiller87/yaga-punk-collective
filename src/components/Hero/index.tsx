ddc384fddbddacfed03603ddfd44e4770b276ca5
Alex Miller <aljmiller87@gmail.com>
2024-03-06 11:01:41 -0500

    hero updates

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,26 +2,32 @@ import React from "react";
 import Image from "next/image";
 import styles from "./Hero.module.scss";
 import BannerImg from "../../../public/assets/images/headshow-nobg.png";
+import FireWorks from "../Fireworks";
 
 const Hero = () => {
   return (
     <div className={styles.Hero} role="banner">
-      <div className={styles.imgWrapper}>
-        <div className={`${styles.text} ${styles.shadow}`}>
-          <p className={styles.title}>Welcome!</p>
-          <h1 className={styles.title}>
-            My name is Emma Curtis and I&apos;m running to be your next
-            Councilmember.
-          </h1>
-        </div>
-        <Image src={BannerImg} alt="Emma Curtis" className={styles.img} />
+      <div className={styles.fireWorks}>
+        <FireWorks />
       </div>
       <div className={styles.text}>
-        <h2 className={styles.btmText}>Join our campaign</h2>
+        {/* <p className={styles.subtitle}>Welcome!</p> */}
+        <h1 className={styles.title}>
+          My name is Emma Curtis and I&apos;m running to be your next
+          Councilmember.
+        </h1>
         <div className={styles.buttons}>
           <a>Donate</a>
           <a>Volunteer</a>
         </div>
+      </div>
+      <div className={styles.imgWrapper}>
+        <Image
+          src={BannerImg}
+          alt="Emma Curtis"
+          className={styles.img}
+          priority
+        />
       </div>
     </div>
   );
