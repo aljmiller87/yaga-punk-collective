88785a0120cde99c7f3452f4434bdc522c6b9e2e
Alex Miller <aljmiller87@gmail.com>
2024-03-08 12:20:40 -0500

    replace home hero image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Hero.module.scss";
-import BannerImg from "../../../public/assets/images/headshot.jpg";
+import BannerImg from "../../../public/assets/images/hero-headshot.jpeg";
 import Link from "next/link";
 
 const Hero = () => {
