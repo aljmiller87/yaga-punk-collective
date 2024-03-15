d3a1115ea5a9784828d845d9cb2160b37bc146f3
Alex Miller <aljmiller87@gmail.com>
2024-03-15 14:25:25 -0400

    testing external image config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+        port: "",
+        pathname: "/4f1cea27-d5e3-49a2-bfa3-40ec76b20287/**",
+      },
+    ],
+  },
+};
