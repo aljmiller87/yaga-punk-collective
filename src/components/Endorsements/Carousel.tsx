486adfdc4b07c15ec03d4154501aa3abf340f216
Alex Miller <aljmiller87@gmail.com>
2024-05-15 15:01:15 -0400

    carousel autoplay

--- src/components/Endorsements/Carousel.tsx
+++ src/components/Endorsements/Carousel.tsx
@@ -47,14 +47,15 @@ const EndorsementsCarousel = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     initialSlide: 0,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -63,7 +64,7 @@ const EndorsementsCarousel = () => {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
