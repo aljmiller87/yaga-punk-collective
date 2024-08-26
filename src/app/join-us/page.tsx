f73256bf20edd43df7b3acf3c9d8b49cf91fb4e1
Alex Miller <aljmiller87@gmail.com>
2024-08-26 11:15:17 -0400

    fixing page file names

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -5,19 +5,18 @@ import Section from "@/components/Section";
 import { getPageData } from "../utils";
 
 export default async function Volunteer() {
-  // const {
-  //   data: {
-  //     pages: { title, bannerImage, bodyText },
-  //   },
-  // } = await getPageData("JoinUs.md");
+  const {
+    data: {
+      pages: { title, bannerImage, bodyText },
+    },
+  } = await getPageData("JoinUs.md");
 
   return (
     <div>
-      <h1>join us page</h1>
-      {/* <PageBanner title={title} img={bannerImage} imgPosition="50% 35%" />
+      <PageBanner title={title} img={bannerImage} imgPosition="50% 35%" />
       <Section>
         <TinaMarkdown content={bodyText} />
-      </Section> */}
+      </Section>
     </div>
   );
 }
