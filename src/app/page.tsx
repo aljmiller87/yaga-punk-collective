06b3a181cbec5528b5f4a61a81784b53746d8c10
rebelrabbits <rebelrabbits20@gmail.com>
2024-11-25 19:00:32 -0500

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Section from "@/components/Section";
 import "../styles/theme.css";
 import VideoGallery from "@/components/VideoGallery";
 import AudioGallery from "@/components/AudioGallery";
+import Link from "next/link";
+import sectionStyles from "../components/Section/section.module.scss";
 
 export const metadata: Metadata = {
-  title: "starter template",
-  description: "DESCRIPTION",
+  title: "Louder Transition Collective",
+  description:
+    "A hardcore anarcho-punk band from Charlotte, NC currently looking for like minded punks to join",
   authors: {
     name: "Alex Miller",
     url: "www.alexmillerwebdev.com",
@@ -20,9 +23,10 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     // url: "https://",
-    title: "STARTER TEMPLATE",
-    description: "STARTER TEMPLATE",
-    siteName: "STARTER TEMPLATE",
+    title: "Louder Transition Collective",
+    description:
+      "A hardcore anarcho-punk band from Charlotte, NC currently looking for like minded punks to join",
+    siteName: "Louder Transition Collective",
     // images: [
     //   {
     //     url: ".jpeg",
@@ -41,9 +45,25 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <Section> {/* <TinaMarkdown content={bodyText} /> */}</Section>
-      {/* <ImageWithGradient /> */}
-      {/* <EndorsementsCarousel /> */}
+      <Section>
+        <p className={sectionStyles.large}>
+          Louder Transition Collective is a hardcore anarcho-punk band from
+          Charlotte, NC. The project was recently started by{" "}
+          <Link href={"/members"}>Rebel Rabbit Alex</Link> who is looking for a
+          guitarist, bassist, and drummer to join. Currently, all bass, guitar,
+          and vocals are produced by Rebel who has the hope others will join to
+          not only make new songs, but also improve the ones already in
+          existence.
+        </p>
+        <p>&nbsp;</p>
+        <h3>Disclaimer:</h3>
+        <p className={sectionStyles.large}>
+          You&apos;ll notice that the songs as the exist are not demo ready. For
+          one, they are lacking drum tracks which are beyond Rebel&apos;s
+          ability. Their current state is only a reflection of the sound and
+          direction Rebel would like to take the band.
+        </p>
+      </Section>
       <AudioGallery title="Songs" addCta />
     </>
   );
